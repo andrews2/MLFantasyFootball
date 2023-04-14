39b545f88f8484f8eff713af12f4220dca5bac6f
andrews2 <andrewjshipman1@gmail.com>
2023-04-13 21:49:54 -0400

    set up db page

--- pages/PlayerDatabase.tsx
+++ pages/PlayerDatabase.tsx
@@ -2,10 +2,18 @@
 * Created by Andrew Shipman
 * 4/12/2023
 */
-import { Input, Layout } from "antd";
+import { Input, Layout, Table, Card, Space } from "antd";
 import { useEffect, useMemo, useState } from "react";
+import { SearchOutlined } from "@ant-design/icons";
+import type { ColumnsType } from 'antd/es/table';
 
 const { Sider, Content } = Layout;
+
+interface TableDataType {
+    key: string;
+    name: string;
+    position: string;
+}
 
 const PlayerDatabase = () => {
     const [players, setPlayers] = useState<Record<string, string>[] | null>(null);
@@ -16,21 +24,51 @@ const PlayerDatabase = () => {
         .then(data => {
             setPlayers(JSON.parse(data))
         })
+    }, []);
+
+    const tableColumns = useMemo((): ColumnsType<TableDataType> => {
+        return [
+            {
+                title: 'Name', 
+                dataIndex: 'name', 
+                key: 'name', 
+            }, 
+            {
+                title: 'Position',
+                dataIndex: 'position',
+                key: 'position',
+                filters: [
+                    {text: 'QB', value: 'QB'},
+                    {text: 'RB', value: 'RB'},
+                    {text: 'WR', value: 'WR'},
+                    {text: 'D/ST', value: 'D/ST'},
+                ],
+                onFilter: (value: string | number | boolean, record: TableDataType) => record.position === value,
+            }
+        ]
     }, [])
 
-    const displayText = useMemo(() => {
+    const tableData = useMemo((): TableDataType[] => {
         if (players) {
-            return players.map(player => {
-                return <p key={player.name}>{`${player.name}, ${player.position}`}</p>
+            return players.map((player, index) => {
+                return {key: String(index), name: player.name, position: player.position}
             })
         }
-        return <p>no data</p>
-    }, [players])
-
+        return [];
+    }, [players]);
 
     return (
-        <Layout>
-
+        <Layout style={{background: '#ffffff', height: '100%'}}>
+            <Sider style={{background: '#ffffff' }} width="25%">
+                <Card title="Filters" style={{ height: '100%' }}>
+                <Space direction="vertical" size={32} style={{ width: '100% '}}>
+                    <Input size="large" prefix={<SearchOutlined />} placeholder="Search..." />
+                </Space>
+                </Card>
+            </Sider>
+            <Content style={{ marginLeft: '16px' }}> 
+                <Table sticky columns={tableColumns} dataSource={tableData}/>
+            </Content>
         </Layout>
     )
 }
--- pages/api/players.ts
+++ pages/api/players.ts
@@ -1,7 +1,7 @@
 import { NextApiRequest, NextApiResponse } from "next";
 
 export default function handler(req: NextApiRequest, res: NextApiResponse) {
-    const testData = [{name: 'Patrick Mahomes', position: 'QB'}, {name: 'Tom Brady', position: 'QB'}]
+    const testData = [{name: 'Patrick Mahomes', position: 'QB'}, {name: 'Tom Brady', position: 'QB'}, {name: 'Deandre Hopkins', position: 'WR'}, {name: 'Aaron Jones', position: 'RB'}]
     const testDataToSend = JSON.stringify(testData);
     res.status(200).json(testDataToSend);
   }
