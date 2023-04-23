08ac298554af42ea2bfb07c2d3956db12b3b5c80
andrews2 <andrewjshipman1@gmail.com>
2023-04-23 13:39:39 -0400

    save chrt as image

--- components/DataVisualization.tsx
+++ components/DataVisualization.tsx
@@ -4,9 +4,11 @@
 */
 
 import { Player } from "@/datatypes/Player";
-import { Card, Divider, Row, Select, Space, Typography } from "antd";
+import { Card, Divider, Dropdown, Row, Select, Space, Typography } from "antd";
 import { useCallback, useEffect, useMemo, useState } from "react";
 import { LineChart, Line, XAxis, YAxis, CartesianGrid, Tooltip, ResponsiveContainer } from 'recharts';
+import { useCurrentPng } from "recharts-to-png";
+import FileSaver from 'file-saver';
 
 const { Text } = Typography;
 
@@ -17,6 +19,7 @@ type DataVisualizationProps = {
 const DataVisualization = ({ player }: DataVisualizationProps) => {
     const [mainStat, setMainStat] = useState('passing_yards');
     const [mainStatMax, setMainStatMax] = useState(50);
+    const [getPng, { ref }] = useCurrentPng();
 
     useEffect(() => {
         if (player?.position === 'RB') {
@@ -60,6 +63,11 @@ const DataVisualization = ({ player }: DataVisualizationProps) => {
         setMainStat(value);
     }, []);
 
+    const onSaveAsImageClick = useCallback(async () => {
+        const png = await getPng();
+        FileSaver.saveAs(png as string, `${player?.name.replaceAll(' ', '_')}_${mainStat}`);
+    }, [getPng, mainStat, player?.name]);
+
     if (!player) {
         return <div />;
     }
@@ -67,26 +75,27 @@ const DataVisualization = ({ player }: DataVisualizationProps) => {
     return (
         <>
             <Card>
-                <Space direction="vertical" style={{ width: '100%' }}>
-                    <Row align="middle">
-                        <Text>Through The Years</Text>
-                        <Divider type="vertical" />
-                        <Select style={{ width: '25%'}} options={lineGraphOptions} onChange={onLineGraphStatChange} value={mainStat.toUpperCase().replaceAll('_', ' ')}/>
-                    </Row>
-                    <ResponsiveContainer width="100%" height={500}>
-                        <LineChart data={chartData}>
-                            <CartesianGrid strokeDasharray="3 3" />
-                            <XAxis dataKey="name" />
-                            <YAxis domain={[0, mainStatMax]}/>
-                            <Tooltip />
-                            <Line dataKey={mainStat.toUpperCase().replaceAll('_', ' ')} />
-                        </LineChart>
-                    </ResponsiveContainer>
-                </Space>
+                <Dropdown menu={{items: [{key: 'save', label:'Save As Image', onClick: onSaveAsImageClick}]}} trigger={['contextMenu']}>
+                    <Space direction="vertical" style={{ width: '100%' }}>
+                        <Row align="middle">
+                            <Text>Through The Years</Text>
+                            <Divider type="vertical" />
+                            <Select style={{ width: '25%'}} options={lineGraphOptions} onChange={onLineGraphStatChange} value={mainStat.toUpperCase().replaceAll('_', ' ')}/>
+                        </Row>
+                        <ResponsiveContainer width="100%" height={500}>
+                            <LineChart data={chartData} ref={ref}>
+                                <CartesianGrid strokeDasharray="3 3" />
+                                <XAxis dataKey="name" />
+                                <YAxis domain={[0, mainStatMax]}/>
+                                <Tooltip />
+                                <Line dataKey={mainStat.toUpperCase().replaceAll('_', ' ')} />
+                            </LineChart>
+                        </ResponsiveContainer> 
+                    </Space>
+                </Dropdown>
             </Card>
             
         </>
-       
     );
 
 };
